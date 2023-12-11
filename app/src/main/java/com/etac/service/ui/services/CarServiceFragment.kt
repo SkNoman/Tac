b65e01302bc29b9d096714c4c3328c6bf0322fbc
Sk Noman <sknoman77@gmail.com>
2023-12-11 22:45:35 +0600

    Working on form UI

--- app/src/main/java/com/etac/service/ui/services/CarServiceFragment.kt
+++ app/src/main/java/com/etac/service/ui/services/CarServiceFragment.kt
@@ -2,6 +2,8 @@ package com.etac.service.ui.services
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
+import com.etac.service.R
 import com.etac.service.base.BaseFragmentWithBinding
 import com.etac.service.databinding.FragmentCarServiceBinding
 
@@ -10,5 +12,6 @@ class CarServiceFragment : BaseFragmentWithBinding<FragmentCarServiceBinding>(
 {
     override fun onViewCreated(view: View , savedInstanceState: Bundle?) {
         super.onViewCreated(view , savedInstanceState)
+        requireActivity().window.statusBarColor = ContextCompat.getColor(requireContext() , R.color.white)
     }
 }
