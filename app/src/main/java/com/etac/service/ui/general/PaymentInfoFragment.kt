b5e803d63ed8002b17b8c21e21158fb9ff6b87c1
Sk Noman <sknoman77@gmail.com>
2023-12-24 01:58:43 +0600

    Update payment info page

---
+++ app/src/main/java/com/etac/service/ui/general/PaymentInfoFragment.kt
@@ -0,0 +1,21 @@
+package com.etac.service.ui.general
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.etac.service.R
+import com.etac.service.base.BaseFragmentWithBinding
+import com.etac.service.databinding.FragmentPaymentInfoBinding
+
+class PaymentInfoFragment : BaseFragmentWithBinding<FragmentPaymentInfoBinding>(
+        FragmentPaymentInfoBinding::inflate
+) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
